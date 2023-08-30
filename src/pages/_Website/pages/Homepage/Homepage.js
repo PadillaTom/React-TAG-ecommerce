2dbfa0908d75ab127e95324d5daa45bb72851c24
Tomas Padilla <padillatomasagustin@gmail.com>
2023-08-30 13:09:11 +0200

    [RTE-3] Rutas y Page OK, ready to begin

--- src/pages/_Website/pages/Homepage/Homepage.js
+++ src/pages/_Website/pages/Homepage/Homepage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Navbar } from "../../components";
 
 const Homepage = () => {
-	return <div>Homepage</div>;
+	return (
+		<main>
+			<Navbar></Navbar>
+			<h2>Home</h2>
+		</main>
+	);
 };
 
 export default Homepage;
